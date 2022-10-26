9fecc6766eb57838518370b0850aaeb14fbc3e46
Thomas Massey <thomassey476@outlook.com>
2022-10-26 14:41:57 +0100

    Changed time

--- subjects/physics/index.js
+++ subjects/physics/index.js
@@ -4,7 +4,7 @@
 // Get a list of how many PNGs are in the folder
 var pngs = [];
 for (var i = 1; i < 117; i++) {
-    pngs.push("subjects/physics/physics_ (" + i + ").png");
+    pngs.push("/physics_ (" + i + ").png");
 }
 
 // Pick a random one
@@ -13,8 +13,8 @@ var randomPng = pngs[Math.floor(Math.random() * pngs.length)];
 // Set the image
 document.getElementById("root").src = randomPng;
 
-// Set the image to be a random one every 5 seconds
+// Set the image to be a random one every 30 seconds
 setInterval(function() {
     var randomPng = pngs[Math.floor(Math.random() * pngs.length)];
     document.getElementById("root").src = randomPng;
-}, 5000);
+}, 30000);
